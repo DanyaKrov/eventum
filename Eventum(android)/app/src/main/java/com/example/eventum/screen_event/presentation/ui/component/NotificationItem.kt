7d31938de2729de00f2eb91eb4e272159ae55743
polina <polina-tokareva-2022@mail.ru>
2025-05-05 19:16:43 +0300

    feature: change 1 event page

--- Eventum(android)/app/src/main/java/com/example/eventum/screen_event/presentation/ui/component/NotificationItem.kt
+++ Eventum(android)/app/src/main/java/com/example/eventum/screen_event/presentation/ui/component/NotificationItem.kt
@@ -1,18 +1,33 @@
 package com.example.eventum.screen_event.presentation.ui.component
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.eventum.screen_event.domain.model.NotificationModel
+import com.example.eventum.ui.theme.Montserrat
+import com.example.eventum.ui.theme.SoftLightOrange
+import com.example.eventum.ui.theme.SoftLightRed
+import com.example.eventum.ui.theme.SoftOrange
+import com.example.eventum.ui.theme.SoftRed
 
 @ExperimentalFoundationApi
 @Composable
@@ -21,23 +36,62 @@ fun NotificationItem(
     onEdit: () -> Unit,
     onDelete: (notification: NotificationModel) -> Unit
 ) {
-    Column(
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = {
-                            onEdit()
-                            },
-                    onDoubleTap = {
-                        onDelete(notification)
-                    }
-                )
-            }
+            .wrapContentHeight()
             .padding(8.dp)
-    ) {
-        Text(text = "Название: ${notification.title}", style = MaterialTheme.typography.bodyLarge)
-        Text(text = "Описание: ${notification.description}", style = MaterialTheme.typography.bodyMedium)
-        Text(text = "Дата: ${notification.time}", style = MaterialTheme.typography.bodySmall)
+//            .background(Color.DarkGray)
+    ){
+        Box(Modifier.fillMaxWidth().background(
+            Brush.horizontalGradient(
+                colors = listOf(
+                    SoftOrange,
+                    SoftLightOrange
+                )
+            )
+        ).padding(16.dp)){
+            Text(text = notification.title,
+                fontFamily = Montserrat,
+                fontWeight = FontWeight.Medium,
+                fontSize = 17.sp,
+                color = Color.DarkGray
+            )
+        }
+        Box(Modifier.fillMaxWidth().background(Color.White)){
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .pointerInput(Unit) {
+                        detectTapGestures(
+                            onTap = {
+                                onEdit()
+                            },
+                            onDoubleTap = {
+                                onDelete(notification)
+                            }
+                        )
+                    }
+                    .padding(16.dp)
+            ) {
+
+//            Text(text = "Описание: ${notification.description}", style = MaterialTheme.typography.bodyMedium)
+                Text(text = "Дата",
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Medium,
+//                fontSize = 17.sp,
+                    color = Color.DarkGray)
+                Text(text = notification.time,
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 17.sp,
+                    color = Color.DarkGray)
+            }
+        }
+
+
     }
+
 }
--- Eventum(android)/app/src/main/java/com/example/eventum/screen_event/presentation/ui/screen/EventScreen.kt
+++ Eventum(android)/app/src/main/java/com/example/eventum/screen_event/presentation/ui/screen/EventScreen.kt
@@ -2,9 +2,12 @@ package com.example.eventum.screen_event.presentation.ui.screen
 
 import android.app.DatePickerDialog
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
@@ -14,9 +17,13 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -25,6 +32,13 @@ import com.example.eventum.screen_event.presentation.event.EventPageEvent
 import com.example.eventum.screen_event.presentation.event.EventPageNavigationEvent
 import com.example.eventum.screen_event.presentation.ui.component.NotificationItem
 import com.example.eventum.screen_event.presentation.viewModel.EventViewModel
+import com.example.eventum.screen_hello.presentation.ui.components.BackButton
+import com.example.eventum.ui.theme.BackGround
+import com.example.eventum.ui.theme.Montserrat
+import com.example.eventum.ui.theme.SoftLightOrange
+import com.example.eventum.ui.theme.SoftLightRed
+import com.example.eventum.ui.theme.SoftOrange
+import com.example.eventum.ui.theme.SoftRed
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -67,30 +81,30 @@ fun EventScreen(
     }
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Детали события") },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        viewModel.handleNavigationEvent(EventPageNavigationEvent.MoveBack())
-                    }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "Назад")
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {
-                        isEditing = true
-                    }) {
-                        Icon(Icons.Default.Edit, contentDescription = "Редактировать")
-                    }
-                }
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { showAddDialog = true }) {
-                Icon(Icons.Default.Add, contentDescription = "Добавить уведомление")
-            }
-        }
+//        topBar = {
+//            TopAppBar(
+//                title = { Text("Детали события") },
+//                navigationIcon = {
+//                    IconButton(onClick = {
+//                        viewModel.handleNavigationEvent(EventPageNavigationEvent.MoveBack())
+//                    }) {
+//                        Icon(Icons.Default.ArrowBack, contentDescription = "Назад")
+//                    }
+//                },
+//                actions = {
+//                    IconButton(onClick = {
+//                        isEditing = true
+//                    }) {
+//                        Icon(Icons.Default.Edit, contentDescription = "Редактировать")
+//                    }
+//                }
+//            )
+//        },
+//        floatingActionButton = {
+//            FloatingActionButton(onClick = { showAddDialog = true }) {
+//                Icon(Icons.Default.Add, contentDescription = "Добавить уведомление")
+//            }
+//        }
     ) { padding ->
         if (eventModel.uiState.isLoading) {
             Box(
@@ -105,6 +119,7 @@ fun EventScreen(
                     .padding(padding)
                     .padding(16.dp)
                     .fillMaxSize()
+//                    .background(BackGround)
             ) {
                 eventModel.event?.let { event ->
                     if (isEditing) {
@@ -159,16 +174,122 @@ fun EventScreen(
                             Text("Сохранить")
                         }
                     } else {
-                        Text(text = "Название: ${event.name}", style = MaterialTheme.typography.titleLarge)
-                        Text(text = "Описание: ${event.description}", style = MaterialTheme.typography.bodyLarge)
-                        Text(text = "Дата: ${event.time}", style = MaterialTheme.typography.bodyMedium)
-                        event.tag?.let {
-                            Text(text = "Тэг: $it", style = MaterialTheme.typography.bodySmall)
+                        Card(
+                            shape = RoundedCornerShape(16.dp),
+                            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight()
+                                .padding(8.dp)
+                                .background(Color.White)
+                        ) {
+                            Box(Modifier.fillMaxWidth().background(
+                                Brush.horizontalGradient(
+                                    colors = listOf(
+                                        SoftRed,
+                                        SoftLightRed
+                                    )
+                                )
+                            )
+                                .padding(16.dp)
+                            )
+                            {
+                                Column {
+                                    Text(
+                                        text = "Название",
+                                        fontFamily = Montserrat,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 17.sp,
+                                        color = Color.White
+                                    )
+                                    Text(text = event.name,
+                                        fontFamily = Montserrat,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 35.sp,
+                                        color = Color.White)
+                                }
+
+                            }
+                            Box(Modifier.fillMaxWidth()
+                                .background(Color.White)
+                            )
+                            {
+                                Column(
+                                    Modifier.fillMaxWidth().padding(16.dp).background(Color.White)
+                                ) {
+                                    Text(
+                                        text = "Описание",
+                                        fontFamily = Montserrat,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 17.sp,
+                                        color = Color.DarkGray
+                                    )
+                                    Text(
+                                        text = event.description,
+                                        fontFamily = Montserrat,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 21.sp,
+                                        color = Color.DarkGray
+                                    )
+                                    Spacer(modifier = Modifier.height(24.dp))
+                                    Text(
+                                        text = "Дата",
+                                        fontFamily = Montserrat,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 17.sp,
+                                        color = Color.DarkGray
+                                    )
+                                    Text(
+                                        text = event.time,
+                                        fontFamily = Montserrat,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 21.sp,
+                                        color = Color.DarkGray
+                                    )
+                                    Spacer(modifier = Modifier.height(24.dp))
+                                    Button(
+                                        onClick = {
+                                            isEditing = true
+                                        },
+                                        modifier = Modifier.fillMaxWidth()
+                                            .background(Brush.horizontalGradient(
+                                                colors = listOf(
+                                                    SoftRed,
+                                                    SoftLightRed
+                                                )),
+                                                shape = MaterialTheme.shapes.extraLarge),
+                                        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+
+                                    ) {
+                                        Text("Редактировать",
+                                            color = Color.White)
+                                    }
+                                }
+                            }
+
+//                            Text(
+//                                text = "Описание: ${event.description}",
+//                                style = MaterialTheme.typography.bodyLarge
+//                            )
+//                            Text(
+//                                text = "Дата: ${event.time}",
+//                                style = MaterialTheme.typography.bodyMedium
+//                            )
+//                            event.tag?.let {
+//                                Text(text = "Тэг: $it", style = MaterialTheme.typography.bodySmall)
+//                            }
+
+
                         }
-
-                        Spacer(modifier = Modifier.height(24.dp))
-
-                        Text("Уведомления:", style = MaterialTheme.typography.titleMedium)
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text("Уведомления",
+                            modifier = Modifier.fillMaxWidth(),
+                            fontFamily = Montserrat,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 21.sp,
+                            color = Color.DarkGray,
+                            textAlign = TextAlign.Center
+                        )
 
                         LazyColumn {
                             items(notificationsModel.notifications) { notification ->
@@ -179,9 +300,29 @@ fun EventScreen(
                                         viewModel.handleEvent(EventPageEvent.DeleteNotification(it))
                                     }
                                 )
-                                Divider()
+//                                Divider()
                             }
                         }
+                        Spacer(modifier = Modifier.height(24.dp))
+                        Button(
+                            onClick = {
+                                showAddDialog = true
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                                .background(Brush.horizontalGradient(
+                                    colors = listOf(
+                                        SoftOrange,
+                                        SoftLightOrange
+                                    )),
+                                    shape = MaterialTheme.shapes.extraLarge),
+                            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+
+                        ) {
+                            Text("Добавить напоминание",
+                                color = Color.DarkGray)
+                        }
+                        BackButton { viewModel.handleNavigationEvent(EventPageNavigationEvent.MoveBack()) }
+
                     }
                 }
 
